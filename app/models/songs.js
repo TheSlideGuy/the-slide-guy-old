d1c1ad7ef2a5cb136e51130d9f94a9768e22dac7
nerdondon <willdon@nerdondon.com>
2015-09-15 00:18:25 -0700

    fix(intellij): Fixed messed up iml from bad commit

--- app/models/songs.js
+++ app/models/songs.js
@@ -1,10 +1,14 @@
 /**
  * Mongoose Schema for the songs collection
- * 
+ *
  */
 
+var indexBy = require('lodash.indexBy');
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+
+// Import the song parts schema
+var songPartSchema = require('./schema_only/song_parts.js');
 
 var songSchema = new Schema({
   title: {type: String, required: true},
@@ -13,16 +17,37 @@ var songSchema = new Schema({
   publisher: String,
   ccli_id: {type: Number, unique: true},
   original_key: String,
-  lyrics: [
-    {
-      song_part: String,
-      body: String
-    }
-  ]
+  lyrics: [songPartSchema]
 });
 
 // Set unique index on artist and title
 songSchema.index({artist: 1, title: 1}, {unique: true});
+
+// Function to ensure that the lyrics array is not empty
+var lyricsValidator = function(lyrArray) {
+  // The lyrics array must be defined and non-empty
+  var songPartsDefined = lyrArr && lyrArr.length > 0;
+  if (!songPartsDefined) {
+    return false;
+  }
+
+  // Make sure that song parts are not empty
+  for (var i = 0; i < lyrArr.length; i++) {
+    if (lyrArr[i].parts && lyrArr[i].parts > 0) {
+      return true;
+    }
+  }
+
+  return false;
+};
+
+// This song must have lyrics or it is invalid
+songSchema.path('lyrics').validate(lyricsValidator, 'Lyrics must be added to the song before it can be saved.');
+
+// Turn the regular lyrics array into a dictionary with song parts as keys
+songSchema.methods.lyricsToDict = function() {
+  return indexBy(this.lyrics, 'song_part');
+};
 
 // Export the model
 module.exports = mongoose.model('Song', songSchema);
