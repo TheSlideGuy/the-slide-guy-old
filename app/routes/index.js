af6dc2f93e28b652bed58b9dbcd0e41a63fadc33
jasonszhao <jasonszhao@yahoo.com>
2015-09-19 21:42:15 -0700

    chore(style): integrate JSCS into IntelliJ

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Welcome to the Slide Guy' });
+  res.render('index.html', { title: 'Welcome to the Slide Guy' });
 });
 
 module.exports = router;
