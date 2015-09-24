33f3f4f07f47c0a76ea5d5b732981df230193e9b
nerdondon <willdon@nerdondon.com>
2015-09-23 23:18:57 -0700

    fix(config.default): Add port to default mongo url

--- app/config/config.defaults.js
+++ app/config/config.defaults.js
@@ -3,7 +3,7 @@ var config = {
   hostname: 'localhost',
   port: '3000',
   mongo: {
-    url: process.env.MONGO_URI || 'localhost',
+    url: process.env.MONGO_URI || 'localhost:27017',
     db: 'slide_guy'
   }
 };
