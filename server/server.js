2533ed6769e049c74468a299225921813e590585
jasonszhao <jasonszhao@yahoo.com>
2015-09-26 15:26:15 -0700

    refactor,feature(server): clean up some stuff and implement better handling
     - re-enable jade
     - use simple jade template to output errors

--- server/server.js
+++ server/server.js
@@ -1,3 +1,9 @@
+/**
+ * The application starts here!
+ *
+ * @see README.md
+ */
+
 var express = require('express');
 var path = require('path');
 
@@ -9,62 +15,55 @@ var bodyParser = require('body-parser');
 
 var config = require('./config');
 
-var routes = require('./routes/index');
 
 var app = express();
 
 app.use(express.static(process.env.NODE_PATH + '/public'));
 
-// view engine setup
-//app.set('views', path.join(__dirname, 'views'));
-//app.set('view engine', 'jade');
+//view engine setup
 
+app.set('views', process.env.NODE_PATH + '/server/views');
+app.set('view engine', 'jade');
+
+//log HTTP requests
 app.use(morgan('dev'));
+
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 
-//app.use(bodyParser.urlencoded({ extended: false }));
-//app.use(cookieParser());
-
+//use router
+var routes = require('./routes/index');
 app.use('/', routes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  var err = Error('Not Found');
   err.status = 404;
   console.log(util.inspect({message: err.message, error: err, file: req.originalUrl}));
   //next(err);
+  res.render('error.jade', {
+    message: err.message,
+    error: err
+  });
+  //res.send('404 Not Found Error');
 });
 
-// ====== error handlers
+//error handler
+
+var error = require('./lib/error');
 
 if (app.get('env') === 'development') {
+
   require('node-monkey').start({
     host: '127.0.0.1',
     port: '50500'
   });
-  app.use(function(err, req, res, next) {
-    res.status(err.status
-      || 500
-    );
 
-    //res.render('error', {
-    //  message: err.message,
-    //  error: err
-    //});
-    console.log(util.inspect({message: err.message, error: err, stack: err.stack}));
-    next(err);
-  });
+  app.use(error.showError);
 }
 
-
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-
-  //res.render('error', {
-  //  message: err.message,
-  //  error: {}
-  //});
-});
+app.use(error.logErrors);
 
 // ====== start listening for HTTP requests!
 
