dda87b4f6e009e60eb999738afd4efa43f903cc3
nerdondon <willdon@nerdondon.com>
2015-11-30 22:27:46 -0800

    chore(deps): Update current deps and add missing deps to config files. Add TS compile options to system.js config

--- public/config.js
+++ public/config.js
@@ -1,27 +1,36 @@
 System.config({
-  baseURL: "/",
+  baseURL: '/',
   defaultJSExtensions: true,
-  transpiler: "typescript",
-  paths: {
-    "npm:*": "vendor/npm/*",
-    "github:*": "vendor/github/*"
+  transpiler: 'typescript',
+  typescriptOptions: {
+    module: 'system',
+    sourceMap: true,
+    removeComments: true,
+    target: 'es5'
   },
-
+  paths: {
+    'npm:*': 'vendor/npm/*',
+    'github:*': 'vendor/github/*'
+  },
   map: {
-    "angular": "github:angular/bower-angular@1.4.7",
-    "angular-material": "github:angular/bower-material@0.11.2",
-    "typescript": "npm:typescript@1.6.2",
-    "github:angular/bower-angular-animate@1.4.7": {
-      "angular": "github:angular/bower-angular@1.4.7"
+    'angular': 'github:angular/bower-angular@1.4.8',
+    'angular-material': 'github:angular/bower-material@1.0.0-rc5-master-db458cb',
+    'angular-mocks': 'github:angular/bower-angular-mocks@1.4.7',
+    'typescript': 'npm:typescript@1.6.2',
+    'github:angular/bower-angular-animate@1.4.8': {
+      'angular': 'github:angular/bower-angular@1.4.8'
     },
-    "github:angular/bower-angular-aria@1.4.7": {
-      "angular": "github:angular/bower-angular@1.4.7"
+    'github:angular/bower-angular-aria@1.4.8': {
+      'angular': 'github:angular/bower-angular@1.4.8'
     },
-    "github:angular/bower-material@0.11.2": {
-      "angular": "github:angular/bower-angular@1.4.7",
-      "angular-animate": "github:angular/bower-angular-animate@1.4.7",
-      "angular-aria": "github:angular/bower-angular-aria@1.4.7",
-      "css": "github:systemjs/plugin-css@0.1.18"
+    'github:angular/bower-angular-mocks@1.4.7': {
+      'angular': 'github:angular/bower-angular@1.4.8'
+    },
+    'github:angular/bower-material@1.0.0-rc5-master-db458cb': {
+      'angular': 'github:angular/bower-angular@1.4.8',
+      'angular-animate': 'github:angular/bower-angular-animate@1.4.8',
+      'angular-aria': 'github:angular/bower-angular-aria@1.4.8',
+      'css': 'github:systemjs/plugin-css@0.1.20'
     }
   }
 });
